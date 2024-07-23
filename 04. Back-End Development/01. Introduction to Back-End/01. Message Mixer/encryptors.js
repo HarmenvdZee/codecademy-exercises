2c65877b2b42b59448ae8ca94c08c831c78a574a
HarmenvdZee <h.w.v.d.zee@student.tue.nl>
2024-07-23 22:03:11 +0200

    message mixer

---
+++ 04. Back-End Development/01. Introduction to Back-End/01. Message Mixer/encryptors.js
@@ -0,0 +1,46 @@
+// Import the encryptors functions here.
+const {
+  caesarCipher,
+  symbolCipher,
+  reverseCipher,
+} = require("./encryptors.js");
+
+const caeserOffset = 12;
+
+const encodeMessage = (str) => {
+  // Use the encryptor functions here.
+  const caeserString = caesarCipher(str, caeserOffset);
+  const symbolString = symbolCipher(caeserString);
+  const reverseString = reverseCipher(symbolString);
+
+  return reverseString;
+};
+
+const decodeMessage = (str) => {
+  // Use the encryptor functions here.
+  const reverseDecoded = reverseCipher(str);
+  const symbolDecoded = symbolCipher(reverseDecoded);
+  const caeserDecoded = caesarCipher(symbolDecoded, -caeserOffset);
+
+  return caeserDecoded;
+};
+
+// User input / output.
+
+const handleInput = (userInput) => {
+  const str = userInput.toString().trim();
+  let output;
+  if (process.argv[2] === "encode") {
+    output = encodeMessage(str);
+  }
+  if (process.argv[2] === "decode") {
+    output = decodeMessage(str);
+  }
+
+  process.stdout.write(output + "\n");
+  process.exit();
+};
+
+// Run the program.
+process.stdout.write("Enter the message you would like to encrypt...\n> ");
+process.stdin.on("data", handleInput);
